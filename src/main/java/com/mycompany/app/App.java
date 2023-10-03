0d891735be70dde072e7dfdc33eef51360fb66d9
Gowthamd0610 <141937898+Gowthamd0610@users.noreply.github.com>
2023-10-03 12:56:09 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World working!";
+    private static final String MESSAGE = "Hello World working1!";
 
     public App() {}
 
